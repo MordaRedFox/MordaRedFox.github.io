0823dac851fb0b51ec91cd41a4b2a519eca25d01
MordaRedFox <kuropatinvlad01@gmail.com>
2026-05-17 19:32:38 +0300

    Added 2 more of my projects - Telegram bot project - Machine learning project

--- js/project_cards.js
+++ js/project_cards.js
@@ -1,11 +1,25 @@
 // Данные о проектах: id, название, краткое описание, язык, ссылка на локальную страницу
 const projectsData = [
     {
-    id: 1,
-    name: "Amateur Site Pizzeria",
-    shortDesc: "Полноценный сайт пиццерии на Django с системой аутентификации и защитой от атак",
-    language: "Django / Python",
-    pageUrl: "project_pizzeria.html"
+        id: 1,
+        name: "Amateur Site Pizzeria",
+        shortDesc: "Полноценный сайт пиццерии на Django с системой аутентификации и защитой от веб-угроз. Есть хорошая админ панель для управления пользователями и меню сайта. Сайт интерактивный и с секретами.",
+        language: "Python / Django",
+        pageUrl: "project_pizzeria.html"
+    },
+    {
+        id: 2,
+        name: "Введение в машинное обучение",
+        shortDesc: "Реализация и анализ четырёх моделей ML: линейная и логистическая регрессия, дерево решений, случайный лес. Предсказание оценок, анализ удовлетворённости, распознавание цифр.",
+        language: "Python / scikit-learn",
+        pageUrl: "project_ml.html"
+    },
+    {
+        id: 3,
+        name: "Telegram Feedback Bot",
+        shortDesc: "Асинхронный Telegram-бот для сбора обратной связи с мультиязычностью, очередью сообщений и админ-панелью в самом боте.",
+        language: "Python / python-telegram-bot",
+        pageUrl: "project_telegram_bot.html"
     }
 ];
 
@@ -18,7 +32,7 @@ function renderProjectCards() {
             <h3>
                 <a href="${project.pageUrl}">${escapeHtml(project.name)}</a>
             </h3>
-            <p class="project-description style="text-align: justify;">${escapeHtml(project.shortDesc)}</p>
+            <p class="project-description" style="text-align: justify;">${escapeHtml(project.shortDesc)}</p>
             <div class="project-meta">
                 <span class="project-language">💻 ${escapeHtml(project.language)}</span>
                 <span class="project-link">
